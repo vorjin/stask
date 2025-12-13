a8de9042cd6270dfc8ddcf5f4f1cc02c1427cbed
Dmytro Varhan <vorjin6272@gmail.com>
2025-12-13 16:24:26 +0200

    mvp cli shell

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,4 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
+// Package cmd is for managing cobra commands
 package cmd
 
 import (
@@ -10,21 +7,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli-task-manager",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "task is a CLI task-manager",
+	Short: "task is a CLI task-manager",
+	Long:  `task is a CLI task-manager`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +34,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
