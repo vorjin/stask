22c7a769aa5d11ffeb079d664544d0bee5a3c5af
Dmytro Varhan <vorjin6272@gmail.com>
2025-12-25 21:35:10 +0200

    handling empty TODO list

--- cmd/list.go
+++ cmd/list.go
@@ -15,13 +15,19 @@ var listCmd = &cobra.Command{
 	Short: "List all of your incomplete tasks",
 	Long:  `List all of your incomplete tasks currently stored in the database.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		color.Magenta("This are your tasks: \n")
 
 		tasks, err := db.ListToDoTasks()
 		if err != nil {
 			fmt.Printf("Error listing TODO tasks. Err: %v", err)
 			os.Exit(1)
 		}
+
+		if len(tasks) == 0 {
+			color.Green("There are currently no tasks to do. Go for a walk :D\n")
+			return
+		}
+
+		color.Magenta("This are your tasks: \n")
 
 		for _, task := range tasks {
 			fmt.Printf("%d. %s\n", task.ID, task.Task)
