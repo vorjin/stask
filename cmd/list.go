af5a061379bfe3af8319063907a01ba604871dd2
Dmytro Varhan <vorjin6272@gmail.com>
2026-03-01 17:39:13 +0200

    unify list cmds

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"stask/model"
 
@@ -11,27 +10,80 @@ import (
 )
 
 func (app *App) NewListCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "list",
-		Short: "List all of your todo tasks",
-		Long:  `List all of your todo tasks currently stored in the database.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			tasks, err := app.DB.ListTasks(model.Todo, 0)
+		Short: "list tasks with a specific status",
+		Long:  "list tasks with a specific status",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			hours, err := cmd.Flags().GetInt("time")
 			if err != nil {
-				fmt.Printf("Error listing TODO tasks. Err: %v", err)
-				os.Exit(1)
+				return err
+			}
+
+			status, err := cmd.Flags().GetString("status")
+			if err != nil {
+				return err
+			}
+
+			var tasks []model.Task
+
+			switch status {
+			case "todo":
+				tasks, err = app.DB.ListTasks(model.Todo, hours)
+			case "completed":
+				tasks, err = app.DB.ListTasks(model.Completed, hours)
+			case "deleted":
+				tasks, err = app.DB.ListTasks(model.Deleted, hours)
+			}
+
+			if err != nil {
+				return err
 			}
 
 			if len(tasks) == 0 {
-				color.Green("There are currently no tasks to do. Go for a walk :D\n")
-				return
+				color.Red("There are no %s tasks! Maaaan, time to do something..\n", status)
+				return nil
 			}
 
-			color.Magenta("This are your tasks: \n")
-
-			for _, task := range tasks {
-				fmt.Printf("%d. %s\n", task.ID, task.Name)
+			switch status {
+			case "todo":
+				color.Green("These are your TODO tasks: \n")
+			default:
+				color.Cyan("These are your %s tasks for the last %d hours: \n", status, hours)
 			}
+			for i, task := range tasks {
+				fmt.Printf("%d. %s\n", i+1, task.Name)
+			}
+
+			return nil
 		},
 	}
+
+	cmd.Flags().IntP("time", "t", 24, "how many hours ago tasks were transitioned to the specified status")
+	f := &statusFlag{"todo", []string{"todo", "completed", "deleted"}}
+	cmd.Flags().VarP(f, "status", "s", "filter tasks by status (todo, completed, deleted)")
+	return cmd
+}
+
+type statusFlag struct {
+	value         string
+	allowedValues []string
+}
+
+func (f *statusFlag) String() string {
+	return f.value
+}
+
+func (f *statusFlag) Type() string {
+	return "string"
+}
+
+func (f *statusFlag) Set(value string) error {
+	for _, v := range f.allowedValues {
+		if v == value {
+			f.value = value
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid value: %s. Allowed values are: %v", value, f.allowedValues)
 }
--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,6 @@ func Execute(database model.TaskStore) {
 	}
 
 	rootCmd.AddCommand(app.NewAddCmd())
-	rootCmd.AddCommand(app.NewCompletedCmd())
 	rootCmd.AddCommand(app.NewDoCmd())
 	rootCmd.AddCommand(app.NewListCmd())
 
